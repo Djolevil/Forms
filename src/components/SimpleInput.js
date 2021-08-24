80c15772f0eab3e6bad885a3ad34efccf2ae13f6
Ђорђе <37450617+Djolevil@users.noreply.github.com>
2021-08-24 22:52:03 +0200

    Add ways for geting user input values by state and ref

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,32 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHasndler = event => {
+    event.preventDefault();
+
+    console.log(enteredName);
+  
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; Not ideal manipulating the DOM directly
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHasndler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
